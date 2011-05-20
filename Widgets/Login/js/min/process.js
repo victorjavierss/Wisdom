d6a41efadf8f7c6d926337e42f2489ccc5d09b52
Victor <victorjavierss@gmail.com>
2011-05-20 10:39:32 -0500

    Se quita soporte SVN

--- Widgets/Login/js/min/process.js
+++
@@ -1,2 +0,0 @@
-
-window.addEvent("domready",function(){$('login').addEvent('submit',function(e){e.stop();$('ajax_loading').setStyle('display','block');$('submit').setStyle('display','none');$(pass_field).value=SHA1($(pass_field).value);this.set('send',{onComplete:function(response){$('ajax_loading').setStyle('display','none');if(response.trim()=='OK'){window.location.reload();}else{var myShake=new Shake('logindiv',{distance:15,duration:80});$('login_response').set('html',response);$('submit').setStyle('display','block');$(pass_field).value="";myShake.shake();}}});this.send();});});
